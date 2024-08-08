4adc3e9a7df103298300c4e0d8f6a354365cb522
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-08-09 01:15:46 +0700

    update footer

--- src/layouts/LayoutHomePage/FooterHomePage.vue
+++ src/layouts/LayoutHomePage/FooterHomePage.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    Footer Home Page
+  <div class="bg-[#dbdade]">
+    <div class="footer-main py-6 flex gap-10">
+      <div>
+        <img src="@/assets/image/background/dbde1c34cf0a3.png" width="200" height="32" alt="">
+      </div>
+      <div class="flex gap-20">
+        <a href="https://www.youtube.com/@KyungminUniv">
+          <img src="@/assets/image/icon/youtube.png" width="15" alt="">
+        </a>
+        <a href="https://www.instagram.com/kyungmin_university/">
+          <img src="@/assets/image/icon/instagram.png" width="20" alt="">
+        </a>
+        <a href="https://www.facebook.com/kmuipsi/">
+          <img src="@/assets/image/icon/facebook.png" width="15" alt="">
+        </a>
+        <a href="https://www.facebook.com/kmuipsi/">
+          <img src="@/assets/image/icon/blog.png" width="20" alt="">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +29,8 @@ export default {
 </script>
 
 <style>
-
+.footer-main {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 </style>
