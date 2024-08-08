f1e458be2b1da2479c7e2028d70e03e6a80add7d
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-08-09 00:49:11 +0700

    update search

--- src/layouts/LayoutHomePage/HeaderHomePage.vue
+++ src/layouts/LayoutHomePage/HeaderHomePage.vue
@@ -24,8 +24,8 @@
 </template>
 
 <script>
-import CardHeader from '@/components/CardHeader.vue';
-import SearchHeader from '@/components/SearchHeader.vue';
+import CardHeader from '@/components/layout/CardHeader.vue';
+import SearchHeader from '@/components/layout/SearchHeader.vue';
 export default {
   components: { CardHeader, SearchHeader },
   name: "HeaderHomePage",
