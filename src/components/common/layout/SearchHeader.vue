be5700557770c577246b834451cd22c895ff1ee4
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-08-12 19:22:35 +0700

    update

--- src/components/common/layout/SearchHeader.vue
+++ src/components/common/layout/SearchHeader.vue
@@ -3,7 +3,20 @@
     <div class="flex flex-col items-end gap-2">
       <form class="max-w-[168px] relative">
         <input class="search-input" type="text" placeholder="Search">
-        <i class="fa-solid fa-magnifying-glass text-white absolute top-1/2 -translate-y-1/2 cursor-pointer right-0"></i>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-4 text-white absolute top-1/2 -translate-y-1/2 cursor-pointer right-0"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
       </form>
       <div class="text-white flex gap-2">
         <div class="text-transform">
