5ec5ed7e561a11910a4280eb59098959e3dea73e
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-08-13 00:24:34 +0700

    update

--- src/components/page/UserPage/CarouselInfomation.vue
+++ src/components/page/UserPage/CarouselInfomation.vue
@@ -1,31 +1,75 @@
 <template>
-  <a-carousel arrows autoplay effect="fade" :dots="false" class="relative" swipeToSlide>
-    <template #prevArrow>
-      <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
-        <LeftCircleOutlined />
-      </div>
-    </template>
-    <template #nextArrow>
-      <div class="custom-slick-arrow" style="right: 10px">
-        <right-circle-outlined />
-      </div>
-    </template>
-    <div><h3>1</h3></div>
-    <div><h3>2</h3></div>
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
-  </a-carousel>
+  <section class="w-full h-[622px] relative">
+    <carousel
+      ref="carousel"
+      :items-to-show="1"
+      :autoplay="3500"
+      :transition="300"
+      :wrapAround="true"
+      :pauseAutoplayOnHover="true"
+      :pagination="{ clickable: true }"
+      class="relative"
+    >
+      <slide v-for="(item, index) in dataBanner" :key="index"
+        >
+        <div class="w-full h-[622px] relative">
+          <img
+            :src="item.slide"
+            alt="slide"
+            class="w-full h-full object-cover"
+          />
+          <img :src="item.description" width="550" class="absolute top-[55%] right-0 z-1 -translate-y-1/2">
+        </div>
+      </slide>
+    </carousel>
+    <div @click="prev" class="absolute top-1/2 left-4 w-[30px] h-[30px] rounded-full border border-white flex justify-center items-center cursor-pointer">
+      <i class="fa-solid fa-chevron-left text-white text-xl"></i>
+    </div>
+    <div @click="next" class="absolute top-1/2 right-4 w-[30px] h-[30px] rounded-full border border-white flex justify-center items-center cursor-pointer">
+      <i class="fa-solid fa-chevron-right text-white text-xl"></i>
+    </div>
+  </section>
 </template>
 
 <script>
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons-vue';
+import { Carousel, Slide } from "vue3-carousel";
 export default {
-  name: 'CarouselInformation',
+  name: "CarouselInformation",
   components: {
-    LeftCircleOutlined,
-    RightCircleOutlined
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      dataBanner: [
+        {
+          slide: 'https://cdn.imweb.me/thumbnail/20240701/e093ddfbddad4.png',
+          description: 'https://cdn.imweb.me/upload/S202307054e561628b31ed/a6547c4041115.png'
+        },
+        {
+          slide: 'https://cdn.imweb.me/thumbnail/20240701/3b0541f7b58e2.jpg',
+          description: 'https://cdn.imweb.me/upload/S202307054e561628b31ed/255d2285fcfeb.png'
+        },
+        {
+          slide: 'https://cdn.imweb.me/thumbnail/20240708/747b7f75f850b.png',
+          description: 'https://cdn.imweb.me/upload/S202307054e561628b31ed/2fcdc69fc4850.png'
+        },
+        {
+          slide: 'https://cdn.imweb.me/thumbnail/20240701/ac1073b62c6f3.png',
+          description: 'https://cdn.imweb.me/upload/S202307054e561628b31ed/3a0107da4f786.png'
+        }
+      ]
+    }
+  },
+  methods: {
+    next() {
+      this.$refs.carousel.next()
+    },
+    prev() {
+      this.$refs.carousel.prev()
+    },
   }
-}
+};
 </script>
 
 <style scoped>
@@ -37,9 +81,9 @@ export default {
   background: #364d79;
   overflow: hidden;
 }
-.anticon-left-circle, .anticon-right-circle {
-  font-size: 30px!important;
-  
+.anticon-left-circle,
+.anticon-right-circle {
+  font-size: 30px !important;
 }
 :deep(.slick-arrow.custom-slick-arrow) {
   width: 25px;
--- src/pages/HomePage/HomeUserPage.vue
+++ src/pages/HomePage/HomeUserPage.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
     <div class="main-container">
-      <info-card-body-scroll class="mb-28"/>
-      <admission-schedule class="mb-20"/>
-      <info-card-body-plus-list class="mb-10"/>
+      <info-card-body-scroll class="mb-28" />
+      <admission-schedule class="mb-20" />
+      <info-card-body-plus-list class="mb-10" />
     </div>
-    <carousel-infomation/>
+    <carousel-infomation />
+    <div class="main-container">
+      <interview-person-card />
+    </div>
+    <media-contents />
+    <campus-gallery/>
+    <faculty-list/>
   </div>
 </template>
 
 <script>
-import InfoCardBodyScroll from '@/components/page/UserPage/InfoCardBodyScroll.vue'
-import InfoCardBodyPlusList from '@/components/page/UserPage/InfoCardBodyPlusList.vue'
-import AdmissionSchedule from '@/components/page/UserPage/AdmissionSchedule.vue'
-import CarouselInfomation from '@/components/page/UserPage/CarouselInfomation.vue'
+import InfoCardBodyScroll from "@/components/page/UserPage/InfoCardBodyScroll.vue";
+import InfoCardBodyPlusList from "@/components/page/UserPage/InfoCardBodyPlusList.vue";
+import AdmissionSchedule from "@/components/page/UserPage/AdmissionSchedule.vue";
+import CarouselInfomation from "@/components/page/UserPage/CarouselInfomation.vue";
+import InterviewPersonCard from "@/components/page/UserPage/InterviewPersonCard.vue";
+import MediaContents from "@/components/page/UserPage/MediaContents.vue";
+import CampusGallery from "@/components/page/UserPage/CampusGallery.vue";
+import FacultyList from '@/components/page/UserPage/FacultyList.vue';
 export default {
-  components: { InfoCardBodyScroll, InfoCardBodyPlusList, AdmissionSchedule, CarouselInfomation },
-  name: 'PageNo1'
-}
+  name: "PageNo1",
+  components: {
+    InfoCardBodyScroll,
+    InfoCardBodyPlusList,
+    AdmissionSchedule,
+    CarouselInfomation,
+    InterviewPersonCard,
+    MediaContents,
+    CampusGallery,
+    FacultyList
+  },
+};
 </script>
 
 <style scoped>
